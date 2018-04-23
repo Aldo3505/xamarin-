65b55b7729785414bd967937ceda04880df36606
Tabilog, Joshua A. T <joshua.a.t.tabilog@accenture.com>
2018-04-23 18:16:02 +0800

    NO ISSUE NO. made design less aggressive

--- Project/zeus-remaster/zeus-android/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ Project/zeus-remaster/zeus-android/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="4c1eec27-8627-41bc-a0d9-66be8049a7d7")
+@BindingBuildInfo(buildId="c4dc94cb-ef31-418c-8a08-7a0db254c054")
 public class DataBindingInfo {}
